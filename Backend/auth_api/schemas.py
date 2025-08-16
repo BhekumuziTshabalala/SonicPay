2db39d82610196b703600b7e846bf63a8edb1800
Bhekumuzi Tshabalala <bhekumuzi.tshabalala@outlook.com>
2025-08-16 12:07:05 +0200

    Adding backend

---
+++ Backend/auth_api/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+class UserRegister(BaseModel):
+    username: str
+    password: str
+    role: str  # "user" or "merchant"
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class Payment(BaseModel):
+    receiver_username: str
+    amount: float
